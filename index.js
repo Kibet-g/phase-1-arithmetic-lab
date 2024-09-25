821fb92680474f128d95e8e83dd925543821e829
+ <kibetg984@gmail.com>
2024-09-25 10:05:36 +0300

    Made changes on the arithmethic code

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+// 2. Random
+let random = Math.floor(Math.random() * 100) + 1;  
+
+// 3. Modulus
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4;  
+
+// 4. Max
+let max = Math.max(1, 5, 10, 20); 
